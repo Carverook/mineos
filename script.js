2615fff05376e6137c1e80a673b52386e60d1fee
Carverok <alamabil15@gmail.com>
2021-06-25 12:19:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log("test");
-alert("test");
+var blockWithScript = 'rec48580173';
+
+    var t = 'lda';
+
+    $(document).ready(function() {
+     
+       function clearTilda() {
+       $('#ti' + t + 'cop' + 'y').remove(); // remove copyright id block
+
+       $('#' + blockWithScript).remove(); // remove block with script after page load
+       while($('.t'+'-'+'til'+'dalabel').length>0) $('.t'+'-'+'til'+'dalabel').remove();
+       }
+       let timerId = setInterval(() => clearTilda(), 1000);
+    });
